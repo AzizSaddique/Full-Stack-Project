2ea588ced5e1ef6856b3c33f3ed7eee09359c221
CodeX60 <151678852+AzizSaddique@users.noreply.github.com>
2025-08-21 09:59:30 -0700

    Sign-in page modification

--- src/helpers/sendVerificationEmails.ts
+++ src/helpers/sendVerificationEmails.ts
@@ -1,1 +1,29 @@
 import { resend } from "@/lib/resend";
+import VerificationEmail from '../../emails/VerificationEmails';
+import { apiResponse } from "@/types/apiResponse";
+export async function sendVerificationEmail(
+
+    email: string,
+    username:string,
+    verifyCode:string
+):Promise<apiResponse>{
+    try {
+        await resend.emails.send({
+  from: 'you@example.com',
+  to: email,
+  subject: 'mstry message| Verification code',
+  react: VerificationEmail({username, otp: verifyCode}),
+});
+        return {
+            success: true,
+            message: 'Verification email sent successfully',
+        };
+    } catch (emailError) {
+        console.error('Error sending verification emails',emailError)
+        return {
+            success: false,
+            message: 'Failed to send verification email',
+        };
+        
+    }
+}
